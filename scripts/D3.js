6358b812f3ad023ec2dbc16e0d874d01fcc2e53a
emanuelecozzolino1926 <emanuele.cozzolino18@gmail.com>
2025-10-01 14:25:06 +0200

    [ADD] calculate total crew mass using a while loop

--- scripts/D3.js
+++ scripts/D3.js
@@ -178,6 +178,16 @@ console.log(eyeColor)
 /* ESERCIZIO 6
   Usa un while loop per calcolare la massa totale dell'equipaggio. Salvala in una variabile chiamata "crewMass".
 */
+let crewMass = 0
+let i = 0
+
+while (i < starWarsCharacters.length) {
+  crewMass += starWarsCharacters[i].mass
+  i++
+}
+
+console.log("Peso totale dell'equipaggio:", crewMass)
+
 
 /* ESERCIZIO 7
   Crea uno if/else statement per rivelare la tipologia di carico, utilizzando la massa totale, di un'ipotetica astronave contenente i personaggi dell'array "starWarsCharacters".
